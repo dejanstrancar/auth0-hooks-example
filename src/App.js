4ef9158087d00945e24be2d1f9cded84e4ec96b5
Dejan Strancar <dejan@apto.si>
2019-06-08 16:41:55 +0200

    initial commit of example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,122 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Auth from "./Auth";
 
-function App() {
+const auth = new Auth();
+
+const MeetupContext = React.createContext();
+const UserContext = React.createContext();
+
+const initialState = {
+  meetup: {
+    title: "Auth0 Online Meetup",
+    date: Date(),
+    attendees: ["Bob", "Jessy", "Christina", "Adam"]
+  },
+  user: {
+    name: "Roy"
+  }
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "subscribeUser":
+      return {
+        ...state,
+        attendees: [...state.attendees, action.payload],
+        subscribed: true
+      };
+    case "unSubscribeUser":
+      return {
+        ...state,
+        attendees: state.attendees.filter(
+          attendee => attendee !== action.payload
+        ),
+        subscribed: false
+      };
+    case "loginUser":
+      return {
+        ...state,
+        isAuthenticated: action.payload.authenticated,
+        name: action.payload.user.name
+      };
+    default:
+      return state;
+  }
+};
+
+const MeetupContextProvider = ({ user, ...props }) => {
+  const [state, dispatch] = React.useReducer(reducer, initialState.meetup);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MeetupContext.Provider
+      value={{
+        ...state,
+        handleSubscribe: () =>
+          dispatch({ type: "subscribeUser", payload: user.name }),
+        handleUnSubscribe: () =>
+          dispatch({ type: "unSubscribeUser", payload: user.name })
+      }}
+    >
+      {props.children}
+    </MeetupContext.Provider>
   );
-}
+};
+
+const UserContextProvider = props => {
+  const [state, dispatch] = React.useReducer(reducer, initialState.user);
+  auth.handleAuthentication().then(() => {
+    dispatch({
+      type: "loginUser",
+      payload: {
+        authenticated: true,
+        user: auth.getProfile()
+      }
+    });
+  });
+  return (
+    <UserContext.Provider value={{ ...state, handleLogin: auth.signIn }}>
+      {props.children}
+    </UserContext.Provider>
+  );
+};
+
+const App = () => (
+  <UserContextProvider>
+    <UserContext.Consumer>
+      {user => (
+        <MeetupContextProvider user={user}>
+          <MeetupContext.Consumer>
+            {meetup => (
+              <div>
+                <h1>{meetup.title}</h1>
+                <span>{meetup.date}</span>
+                <div>
+                  <h2>{`Attendees (${meetup.attendees.length})`}</h2>
+                  {meetup.attendees.map(attendant => (
+                    <li>{attendant}</li>
+                  ))}
+                  <p>
+                    {user.isAuthenticated ? (
+                      !meetup.subscribed ? (
+                        <button onClick={meetup.handleSubscribe}>
+                          Subscribe
+                        </button>
+                      ) : (
+                        <button onClick={meetup.handleUnSubscribe}>
+                          Unsubscribe
+                        </button>
+                      )
+                    ) : (
+                      <button onClick={user.handleLogin}>Login</button>
+                    )}
+                  </p>
+                </div>
+              </div>
+            )}
+          </MeetupContext.Consumer>
+        </MeetupContextProvider>
+      )}
+    </UserContext.Consumer>
+  </UserContextProvider>
+);
 
 export default App;
